ad45dd1662cfce70e3e1f7f45b9a9afa8b44d06a
Akarsh Vyas <akarshvyas100@gmail.com>
2026-05-23 14:47:30 +0530

    Added new files

---
+++ chapter13.py
@@ -0,0 +1,23 @@
+#      #   0.      1.      2.   3
+# a = ["monday","tuesday",123,567,123,123,123]
+
+# tup = tuple(a)
+
+# print(tup[-1])
+
+# #it has immutable nature - you cannot change any values inside your tuple 
+
+# print(tup.index("tuesday"))
+
+# print(tup.count(123))
+
+
+# def students():
+#     return "akarsh",24,"akarsh@gmail.com"
+
+# info = students()
+
+# name , age , mail = info
+
+# print(name)
+
